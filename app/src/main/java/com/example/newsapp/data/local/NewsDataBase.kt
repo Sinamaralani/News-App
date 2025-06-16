c7e6e7094af91eac9f94a7427878e7920dc5e0ce
Sinamaralani <mr.sinamaralani@gmail.com>
2025-06-16 16:20:08 +0330

    Final Commit

--- app/src/main/java/com/example/newsapp/data/local/NewsDataBase.kt
+++ app/src/main/java/com/example/newsapp/data/local/NewsDataBase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.newsapp.domain.model.Article
 
-@Database(entities = [Article::class], version = 2)
+@Database(entities = [Article::class], version = 2, exportSchema = false)
 @TypeConverters(NewsTypeConverter::class)
 abstract class NewsDataBase : RoomDatabase() {
     abstract val newsDao: NewsDao
